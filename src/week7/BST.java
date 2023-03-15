0fb5005e77894f5e9d1dd2ed5860e2c8e16efffa
Jesse Hartloff <hartloff@buffalo.edu>
2023-03-15 09:01:31 -0400

    BST example

--- src/week7/BST.java
+++ src/week7/BST.java
@@ -108,9 +108,28 @@ public class BST<A> {
         System.out.println(bst.root.inOrderTraversal(bst.root));
     }
 
+    private static void example3() {
+        BST<Integer> bst = new BST<>(new IntIncreasing());
+
+        bst.insert(-3);
+        bst.insert(2);
+        bst.insert(4);
+        bst.insert(5);
+        bst.insert(7);
+        bst.insert(8);
+        bst.insert(14);
+
+        System.out.println(bst.find(7));
+        System.out.println(bst.find(8));
+        System.out.println(bst.find(3));
+
+        System.out.println(bst.root.inOrderTraversal(bst.root));
+    }
+
     public static void main(String[] args) {
         example1();
         example2();
+        example3();
     }
 
 }
