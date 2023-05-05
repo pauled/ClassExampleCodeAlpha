845f9912781c4424404758390b394fbec32ee243
jessehartloff <hartloff@buffalo.edu>
2023-05-05 13:54:41 -0400

    1pm lecture example

---
+++ src/week13/jessesway1pm/DoubleIncreasing.java
@@ -0,0 +1,11 @@
+package week13.jessesway1pm;
+
+import week6.Comparator;
+
+public class DoubleIncreasing extends Comparator<Double> {
+
+    @Override
+    public boolean compare(Double a, Double b) {
+        return a < b;
+    }
+}
