79895ee677d645691237f06e6a1c9196f4cf29d8
jessehartloff <hartloff@buffalo.edu>
2023-10-20 12:32:28 -0400

    add structure for live coding example

---
+++ src/week8/treesum/SumTests.java
@@ -0,0 +1,12 @@
+package week8.treesum;
+
+import org.junit.Test;
+
+
+public class SumTests {
+    @Test
+    public void testTreeSum() {
+
+
+    }
+}
---
+++ src/week8/treesum/TreeSum.java
@@ -0,0 +1,17 @@
+package week8.treesum;
+
+import week8.BinaryTreeNode;
+
+public class TreeSum {
+
+    /**
+     * Returns the sum of all the values in the tree
+     *
+     * @param tree the root node of the tree
+     * @return the sum of all values in the given tree
+     */
+    public static double treeSum(BinaryTreeNode<Double> tree) {
+        return 0.0;
+    }
+
+}
