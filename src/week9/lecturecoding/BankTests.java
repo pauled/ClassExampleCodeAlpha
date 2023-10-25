ab450007cfc8b37a97eef25e1288a414a70f4009
jessehartloff <hartloff@buffalo.edu>
2023-10-25 13:06:15 -0400

    add BankAccount tests

--- src/week9/lecturecoding/BankTests.java
+++ src/week9/lecturecoding/BankTests.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 
 public class BankTests {
@@ -23,10 +23,28 @@ public class BankTests {
         assertEquals(106.5, account.checkAccount(), 0.001);
 
         account.withdraw(10.0);
-        assertEquals(1.5, account.checkAccount(), 0.001);
+        assertEquals(96.5, account.checkAccount(), 0.001);
 
+        BankAccount myAccount = new BankAccount("Jesse", 19.95);
 
+        assertEquals(1.5, myAccount.checkAccount(), 19.95);
+
+        account.transfer(myAccount, 50.55);
+
+        assertEquals(45.95, account.checkAccount(), 0.001);
+        assertEquals(70.5, myAccount.checkAccount(), 0.001);
+
+        assertFalse(account.isOverdrawn());
+        assertFalse(myAccount.isOverdrawn());
+
+        account.withdraw(50.0);
+        assertEquals(-4.05, account.checkAccount(), 0.001);
+        assertTrue(account.isOverdrawn());
     }
 
+    @Test
+    public void testBank(){
+
+    }
 
 }
