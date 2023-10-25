2db6a0a2bca7e0b64dea8b00650587e7e69350c0
jessehartloff <hartloff@buffalo.edu>
2023-10-25 12:58:45 -0400

    javadocs

--- src/week9/lecturecoding/Bank.java
+++ src/week9/lecturecoding/Bank.java
@@ -10,6 +10,12 @@ public class Bank {
     }
 
 
+    /**
+     * Returns all the accounts that have a large amount of money
+     * @param threshold the amount that defines "large"
+     * @return An ArrayList of all BankAccounts containing more than
+     *         threshold dollars
+     */
     public ArrayList<BankAccount> getLargeAccounts(double threshold){
         return null;
     }
