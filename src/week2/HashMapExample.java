d887bd10120dfb0313f22a94a343a1a2d14d5502
Jesse Hartloff <hartloff@buffalo.edu>
2024-09-04 12:11:43 -0400

    Coleman

--- src/week2/HashMapExample.java
+++ src/week2/HashMapExample.java
@@ -7,7 +7,7 @@ public class HashMapExample {
         HashMap<String, Integer> bills = new HashMap<>();
 
         bills.put("Allen", 17);
-        bills.put("Diggs", 14);
+        bills.put("Coleman", 0);
         System.out.print("What is Allen's number? ");
         System.out.println(bills.get("Allen"));
         System.out.println(bills);
